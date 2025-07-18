e1d8a41818ea63d591406d5ae6b0030c7dec2ad4
Olivier PERDRIX <olivierperdrix@live.fr>
2025-07-18 11:02:38 +0200

    feat: update homepage and add new pages for contact, projects, and skills

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ProjectsPage() {
+  return (
+    <main>
+      <h1>Mes projets</h1>
+      <p>Découvrez ici une sélection de mes réalisations professionnelles et personnelles.</p>
+    </main>
+  );
+} 
